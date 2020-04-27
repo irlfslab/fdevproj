f1e71b1717c2adb3e11bc76cc4603be74f1c8b5d
skyastro <53288945+skyastro@users.noreply.github.com>
2020-04-27 15:41:38 -0400

    Update jq_signup.js

--- assets/scripts/jq_signup.js
+++ assets/scripts/jq_signup.js
@@ -1,38 +1,36 @@
 var is_valid = true;
 function validate(vin, vtype) {
   if (vtype == "name"){
-     var re = /^[a-zA-Z]{2,20}$/     //validate name pattern with 2 - 20 characters
+     var re = /^[a-zA-Z]{2,20}$/     
     }
   else if (vtype == "email") {
-     var re = /[a-zA-Z0-9._-]@[a-zA-Z0-9-]+\.\w+(\.\w+)?/       //validate email pattern
+     var re = /[a-zA-Z0-9._-]@[a-zA-Z0-9-]+\.\w+(\.\w+)?/       
     }
     else {
-        var re = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/      //validate phone 
+        var re = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/      
     }
    return is_valid=re.test(vin)
 }
 
 $(document).ready(function() {
-//clear signup form
+//reset signup form
 $('#signbtn').click(function(event) {
     $("#signup")[0].reset();
 });
 
 //Validate signup form
-//validate firstname 
 $('#signup_fname').on('input', function() {
     var val=$(this).val();
     validate(val, "name")
 	if(is_valid){
         $(this).removeClass("invalid").addClass("valid");
-        $(this).siblings('div').addClass("msg");      //clear any message
+        $(this).siblings('div').addClass("msg");      
     }
 	else{
         $(this).removeClass("valid").addClass("invalid");
     }
 });
 
-//validate lastname
 $('#signup_lname').on('input', function() {
     var val=$(this).val();
     validate(val, "name")
@@ -45,7 +43,6 @@ $('#signup_lname').on('input', function() {
     }
 });
 
-//validate phone
 $('#signup_phone').on('input', function() {
     var val=$(this).val();
     validate(val, "phone")
@@ -58,7 +55,6 @@ $('#signup_phone').on('input', function() {
     }       
 });
 
-//validate email
 $('#signup_email').on('input', function() {
     var val=$(this).val();
     validate(val, "email")
@@ -71,7 +67,6 @@ $('#signup_email').on('input', function() {
     }
  });
 
- //validate password
  $('#signup_password').on('input', function() {
     if ($(this).val().length >= 8) {
         $(this).removeClass("invalid").addClass("valid");
@@ -82,11 +77,9 @@ $('#signup_email').on('input', function() {
     }
  });
 
- //validate repeat password
  $('#signup_repassword').on('input', function() {
     var password = $("#signup_password").val();
     var repassword = $("#signup_repassword").val();
-    // Check for equality with the password inputs
     if (password == repassword ) {
         $(this).removeClass("invalid").addClass("valid");
         $(this).siblings('div').addClass("msg");
@@ -94,7 +87,7 @@ $('#signup_email').on('input', function() {
         $(this).removeClass("valid").addClass("invalid");
     }
  });
-
+// Form submit
 $("#signup").on('submit', function(){
 	var form_data=$("#signup").serializeArray();
 	var error_free=true;
