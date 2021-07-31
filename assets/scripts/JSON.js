ca7742f70f72552b3f5afd86ebaa583305c7680e
IL <53288945+irlfslab@users.noreply.github.com>
2021-07-31 18:15:29 -0400

    Update JSON.js

--- assets/scripts/JSON.js
+++ assets/scripts/JSON.js
@@ -1,7 +1,7 @@
 $(document).ready(function(data){
  $("#getJsonSrc").click(function() {
    //get JSON file
- var jqxhr = $.getJSON( "https://ilfslab.github.io/fdevproj/assets/files/class.json", function(data) {
+ var jqxhr = $.getJSON( "https://irlfslab.github.io/fdevproj/assets/files/class.json", function(data) {
     var class_data = '';
     var head_count = 0;
     $('#tdata td').remove();
